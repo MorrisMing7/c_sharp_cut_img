195d62f1c0fb368e8cc35e818219ee3b57000d2c
morris <morris_ming@163.com>
2018-04-09 10:26:45 +0800

    添加文件名与右键加滚轮事件

---
+++ 切/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace 切
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openFileToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoomRateTextBox = new System.Windows.Forms.ToolStripTextBox();
+            this.cutSizeTextBox = new System.Windows.Forms.ToolStripTextBox();
+            this.fileNameTextBox = new System.Windows.Forms.ToolStripTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picBox
+            // 
+            this.picBox.BackColor = System.Drawing.Color.Thistle;
+            this.picBox.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.picBox.ImageLocation = "";
+            this.picBox.Location = new System.Drawing.Point(223, 46);
+            this.picBox.Margin = new System.Windows.Forms.Padding(0);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(531, 347);
+            this.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picBox.TabIndex = 0;
+            this.picBox.TabStop = false;
+            this.picBox.Paint += new System.Windows.Forms.PaintEventHandler(this.picBox_Paint);
+            this.picBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.picBox_MouseClick);
+            this.picBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PicBox_MouseDown);
+            this.picBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PicBox_MouseMove);
+            this.picBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PicBox_MouseUp);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem2,
+            this.zoomRateTextBox,
+            this.cutSizeTextBox,
+            this.fileNameTextBox});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1116, 27);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openFileToolStripMenuItem2
+            // 
+            this.openFileToolStripMenuItem2.Name = "openFileToolStripMenuItem2";
+            this.openFileToolStripMenuItem2.Size = new System.Drawing.Size(68, 23);
+            this.openFileToolStripMenuItem2.Text = "打开文件";
+            this.openFileToolStripMenuItem2.Click += new System.EventHandler(this.openFileToolStripMenuItem2_Click);
+            // 
+            // zoomRateTextBox
+            // 
+            this.zoomRateTextBox.Name = "zoomRateTextBox";
+            this.zoomRateTextBox.ReadOnly = true;
+            this.zoomRateTextBox.Size = new System.Drawing.Size(320, 23);
+            // 
+            // cutSizeTextBox
+            // 
+            this.cutSizeTextBox.Name = "cutSizeTextBox";
+            this.cutSizeTextBox.Size = new System.Drawing.Size(110, 23);
+            this.cutSizeTextBox.Text = "Input cutsize here";
+            this.cutSizeTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.cutSizeTextBox_KeyDown);
+            // 
+            // fileNameTextBox
+            // 
+            this.fileNameTextBox.Name = "fileNameTextBox";
+            this.fileNameTextBox.ReadOnly = true;
+            this.fileNameTextBox.Size = new System.Drawing.Size(600, 23);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1116, 761);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.picBox);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripTextBox zoomRateTextBox;
+        private System.Windows.Forms.ToolStripTextBox cutSizeTextBox;
+        private System.Windows.Forms.ToolStripTextBox fileNameTextBox;
+    }
+}
+
---
+++ 切/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Reflection;
+
+namespace 切
+{
+    public partial class Form1 : Form
+    {
+        private Point mouseDownPoint;
+        private bool isSelected;
+        private bool opened=false;
+        private bool isChangingSize = false;
+        private Bitmap open_img;
+        private Bitmap picBox_img;
+        private Bitmap cursor_img;
+        private int cut_size = 512;
+        private double zoom_rate=1;
+        private string last_open_dir = "";
+        private string save_cut_img_dir = "";
+        public Form1()
+        {
+            InitializeComponent();
+            this.picBox.MouseWheel += picBox_MouseWheel;
+            cursor_img = new Bitmap(@"C:\Users\Morris\Documents\Visual Studio 2013\Projects\切\切\cursor.png");
+
+        }
+        public void SetCursor(Bitmap cursor, int tSize)
+        {
+            
+            Bitmap myNewCursor = new Bitmap(tSize, tSize);
+            Graphics g = Graphics.FromImage(myNewCursor);
+            g.Clear(Color.FromArgb(0, 0, 0, 0));
+            g.DrawImage(cursor, 0, 0, tSize, tSize);
+            this.picBox.Cursor = new Cursor(myNewCursor.GetHicon());
+            g.Dispose();
+            myNewCursor.Dispose();
+        }
+        public Bitmap im_resize(Bitmap img, int width, int height)
+        {
+            Bitmap result = new Bitmap(width, height);
+            Graphics g = Graphics.FromImage(result);
+            g.Clear(Color.White);
+            g.DrawImage(img, 0, 0, width, height);
+            g.Dispose();
+            return result;
+        }
+        public void reDrawPicBoxImg(int width=-1,int height=-1)
+        {
+            width = width == -1 ? Convert.ToInt32(Math.Ceiling(open_img.Width*zoom_rate) ): width;
+            height = height == -1 ? Convert.ToInt32(Math.Ceiling(open_img.Height * zoom_rate)) : height;
+            picBox.Height = height;
+            picBox.Width = width;
+            picBox.Image = im_resize(picBox_img, width, height);
+        }
+        void picBox_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (!opened) return;
+            if (isChangingSize )
+            {
+                if (cut_size % 256 != 0) cut_size = cut_size / 256 * 256;
+                int tmpt = e.Delta > 0 ? cut_size + 256 : cut_size - 256;
+                if (tmpt < 256 || tmpt > 2048) return;
+                cut_size = tmpt;
+                this.cutSizeTextBox.Text = "cut_size" + cut_size;
+                SetCursor(cursor_img, Convert.ToInt32(Math.Ceiling(cut_size * zoom_rate)));
+                return;
+            }
+            int Height=0;
+            int Width=0;
+            double tmp;
+            tmp= e.Delta > 0 ? zoom_rate +0.1 : zoom_rate -0.1;
+            if (tmp > 2 || tmp < 0.05) return;
+            else { zoom_rate = tmp;  }
+            Height = Convert.ToInt32(Math.Ceiling(open_img.Height * zoom_rate));
+            Width = Convert.ToInt32(Math.Ceiling(open_img.Width * zoom_rate));
+            if (Height < 200){ return;}
+            SetCursor(cursor_img, Convert.ToInt32(Math.Ceiling(cut_size * zoom_rate)));
+            reDrawPicBoxImg(Width, Height);
+            zoomRateTextBox.Text = "img_size:"+open_img.Width+"*"+open_img.Height+"   zoomrate:" + zoom_rate+ "  picBox:"+Width+"*"+Height;
+        }
+        
+        private void openFileToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = last_open_dir==""?System.IO.Path.GetDirectoryName(this.GetType().Assembly.Location):last_open_dir;
+            openFileDialog.Filter = "png文件|*.png;*.jpg";
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.FilterIndex = 1;
+            string fName = "";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fName = openFileDialog.FileName;
+                last_open_dir = System.IO.Path.GetDirectoryName(fName);
+                save_cut_img_dir = System.IO.Path.GetDirectoryName(this.GetType().Assembly.Location) + "/" + System.IO.Path.GetFileName(fName);
+                if (!System.IO.Directory.Exists(save_cut_img_dir))
+                {
+                    System.IO.Directory.CreateDirectory(save_cut_img_dir);
+                }
+                open_img = new Bitmap(fName);
+                picBox.Top = 0;
+                picBox.Left = 0;
+                this.picBox.Width = open_img.Width;
+                this.picBox.Height = open_img.Height;
+                this.picBox_img = new Bitmap(open_img);
+                this.picBox.Image = this.picBox_img;
+                zoom_rate = 1;
+                SetCursor(cursor_img, cut_size);
+                zoomRateTextBox.Text =  "img_size:"+open_img.Width+"*"+open_img.Height+"   zoomrate:" + zoom_rate+ "  picBox:"+Width+"*"+Height;
+                opened = true;
+                fileNameTextBox.Text =  System.IO.Path.GetFileName(fName);
+            }
+        }
+
+        Point get_Boundary(int x, int y)
+        {
+            int top = y - cut_size / 2;
+            int left = x - cut_size / 2;
+            if(top>0 && left>0 && left+cut_size<open_img.Width &&top + cut_size < open_img.Height)
+            {
+                return new Point(left, top);
+            }
+            return new Point(-1,-1);
+        }
+        private void picBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (!opened) return;
+            if (e.Button == MouseButtons.Left)
+            {
+                Point mouseLoc=this.PointToClient(Control.MousePosition);
+                int tmp_x = mouseLoc.X - picBox.Left;
+                int tmp_y = mouseLoc.Y - picBox.Top;
+                int X = Convert.ToInt32(Math.Ceiling((tmp_x) / zoom_rate));
+                int Y = Convert.ToInt32(Math.Ceiling((tmp_y) / zoom_rate));
+                Point left_top = get_Boundary(X, Y);
+                if (left_top.X != -1)
+                {
+                    Bitmap cut_img = new Bitmap(cut_size, cut_size);
+                    Graphics g = Graphics.FromImage(cut_img);
+                    g.DrawImage(open_img,0,0,new Rectangle(left_top.X,left_top.Y,cut_size,cut_size),GraphicsUnit.Pixel);
+
+                    Graphics g2 = Graphics.FromImage(picBox_img);
+                    g2.DrawLines(new Pen(Color.Gold, 4), new Point[] { new Point(left_top.X,left_top.Y), new Point(left_top.X, left_top.Y + cut_size), new Point(left_top.X + cut_size, left_top.Y + cut_size), new Point(left_top.X + cut_size, left_top.Y),new Point(left_top.X , left_top.Y ) });
+                    reDrawPicBoxImg();
+
+                    Image saveImage = Image.FromHbitmap(cut_img.GetHbitmap());
+                    string name = DateTime.Now.ToString("hh：mm：ss");
+                    name = save_cut_img_dir+"/" + name+ "x.jpg";
+                    saveImage.Save(name, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    saveImage.Dispose();
+                    cut_img.Dispose();
+                    g.Dispose();
+                    return;
+                }
+                string tmp = "";
+                tmp += X + " " +Y;
+                MessageBox.Show(tmp);
+            }
+        }
+        private void PicBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                mouseDownPoint.X = Cursor.Position.X;
+                mouseDownPoint.Y = Cursor.Position.Y;
+                isSelected = true;
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                isChangingSize = true;
+            }
+        }
+
+        private void PicBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isSelected)
+            {
+                this.picBox.Left = this.picBox.Left + (Cursor.Position.X - mouseDownPoint.X);
+                this.picBox.Top = this.picBox.Top + (Cursor.Position.Y - mouseDownPoint.Y);
+                mouseDownPoint.X = Cursor.Position.X;
+                mouseDownPoint.Y = Cursor.Position.Y;
+            }
+        }
+
+        private void PicBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            isSelected = false;
+            if (e.Button == MouseButtons.Right)
+            {
+                isChangingSize = false;
+            }
+        }
+
+        private void picBox_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, picBox.ClientRectangle, Color.Red, ButtonBorderStyle.Solid);
+        }
+
+        private void cutSizeTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!opened)
+            {
+                MessageBox.Show("please open an image first");
+                return;
+            }
+            if (e.KeyCode == Keys.Enter)
+            {
+                try
+                {
+                    cut_size =Convert.ToInt32(cutSizeTextBox.Text);
+                    cutSizeTextBox.Text = "cut_size:" + cut_size;
+                }
+                catch
+                {
+                    MessageBox.Show("please input pure number");
+                    this.cutSizeTextBox.Text = "cut_size:512(default)";
+                    this.SetCursor(cursor_img,  Convert.ToInt32(Math.Ceiling(512 * zoom_rate)));
+                }
+                if(cut_size>picBox_img.Height || cut_size > picBox_img.Width ||cut_size<27)
+                {
+                    MessageBox.Show("cutsize is too big or too small, it should less than " + (picBox_img.Height < picBox_img.Width ? picBox_img.Height : picBox_img.Width)+"and bigger than 27");
+                    this.cutSizeTextBox.Text = "cut_size:512(default)";
+                    cut_size = 512;
+                }
+                this.SetCursor(cursor_img,  Convert.ToInt32(Math.Ceiling(cut_size * zoom_rate)));
+
+            }
+        }
+
+
+    }
+}
